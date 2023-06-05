7644a5b1de871a8053bbe732c81d279e7e540778
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-06-06 01:35:05 +0500

    add client, HomePage and RegistrationPage fields, integrated React

---
+++ client/client.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+const Home = () => <h1>Домашняя страница</h1>;
+const About = () => <h1>О нас</h1>;
+const Contact = () => <h1>Контакты</h1>;
+
+const App = () => (
+    <Router>
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Главная</Link>
+                </li>
+                <li>
+                    <Link to="/about">О нас</Link>
+                </li>
+                <li>
+                    <Link to="/contact">Контакты</Link>
+                </li>
+            </ul>
+        </nav>
+
+        <Switch>
+            <Route exact path="/">
+                <Home />
+            </Route>
+            <Route path="/about">
+                <About />
+            </Route>
+            <Route path="/contact">
+                <Contact />
+            </Route>
+        </Switch>
+    </Router>
+);
+
+export default App;
