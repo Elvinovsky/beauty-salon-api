27487addee2e19a73562dd7ca3ef45568a4c95fc
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-06-06 04:55:27 +0500

    refactoring

--- client/client.js
+++ client/client.js
@@ -1,48 +1,101 @@
-import React from 'react'
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import * as PropTypes from "prop-types";
+const AppointmentForm = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [selectedDate, setSelectedDate] = useState(null);
+    const [service, setService] = useState('');
+    const [price, setPrice] = useState('');
 
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
 
-const Home = () => <h1>Домашняя страница</h1>;
-const About = () => <h1>О нас</h1>;
-const Contact = () => <h1>Контакты</h1>;
+        // Создание объекта с данными клиента
+        const newCustomer = {
+            name,
+            email,
+            date: selectedDate,
+            service,
+            price
+        };
 
-function Switch(props) {
-    return null;
-}
+        // Отправка запроса на сервер
+        fetch('/customers', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newCustomer)
+        })
+            .then(response => {
+                if (response.ok) {
+                    console.log('Клиент успешно сохранен');
+                    // Дополнительная логика или обновление состояния при успешном сохранении клиента
+                } else {
+                    console.error('Ошибка при сохранении клиента');
+                }
+            })
+            .catch(error => {
+                console.error('Ошибка при отправке запроса:', error);
+            });
+    };
 
-Switch.propTypes = {children: PropTypes.node};
-
-const App = () => (
-    <Router>
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Главная</Link>
-                </li>
-                <li>
-                    <Link to="/about">О нас</Link>
-                </li>
-                <li>
-                    <Link to="/contact">Контакты</Link>
-                </li>
-            </ul>
-        </nav>
-
-        <Switch>
-            <Route exact path="/">
-                <Home />
-            </Route>
-            <Route path="/about">
-                <About />
-            </Route>
-            <Route path="/contact">
-                <Contact />
-            </Route>
-        </Switch>
-    </Router>
+    return (
+        <div>
+            <h2>Запись на прием</h2>
+            <form onSubmit={handleFormSubmit}>
+                <div>
+                    <label htmlFor="name">Имя:</label>
+                    <input
+                        type="text"
+                        id="name"
+                        value={name}
+                        onChange={e => setName(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="date">Дата:</label>
+                    <DatePicker
+                        id="date"
+                        selected={selectedDate}
+                        onChange={date => setSelectedDate(date)}
+                        dateFormat="dd.MM.yyyy"
+                        placeholderText="Выберите дату"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="service">Услуга:</label>
+                    <input
+                        type="text"
+                        id="service"
+                        value={service}
+                        onChange={e => setService(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="price">Цена:</label>
+                    <input
+                        type="text"
+                        id="price"
+                        value={price}
+                        onChange={e => setPrice(e.target.value)}
+                    />
+                </div>
+                <button type="submit">Записаться</button>
+            </form>
+        </div>
     );
+};
 
-export default App;
+export default AppointmentForm;
