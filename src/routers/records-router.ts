c7c84527faa430aaa9b24c7061af597bf8c71b17
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-06-25 17:56:02 +0500

    create File 'records-controllers.ts' and transition to OOP

--- src/routers/records-router.ts
+++ src/routers/records-router.ts
@@ -14,6 +14,53 @@ import { checkForErrors } from "../midllewares/express-validator/check-errors";
 
 export const RecordsRouter = Router()
 
+export class RecordController {
+    async getRecordsByAdmin ( req: Request, res: Response ) {
+        // todo superAdminGuard
+        const allRecords = await recordsRepo.getAllRecords()
+
+        if (allRecords) {
+            res.send(allRecords)
+            return
+        }
+
+        res.sendStatus(500)
+        return
+    }
+
+    async getRecordsByUser ( req: Request, res: Response ) {
+        const recordByPhone = await recordsRepo.getRecordsByPhone(req.params.phoneNumber)
+
+        if (recordByPhone) {
+            res.send(recordByPhone)
+            return
+        }
+
+        res.sendStatus(500)
+        return
+    }
+
+    async updateRecordForUser ( req: Request, res: Response ) {
+        const changeRecord = await recordsRepo.updateRecord(Number(req.params.phoneNumber),
+            req.body?.serviceTitle,
+            req.body?.dateMeeting,
+            req.body?.userName,
+            req.body?.instagram)
+
+        if (changeRecord === null) {
+            res.sendStatus(500)
+            return
+        }
+        if (changeRecord) {
+            res.sendStatus(204)
+            return
+        }
+        res.sendStatus(404)// todo перед отправкой смс должна пройти валидация на существование номера в БД
+        return
+    }
+}
+
+//админ получает все данные о записях клиентов
 RecordsRouter.get('/',
     async( req: Request, res: Response ) => {// todo superAdminGuard
         const allRecords = await recordsRepo.getAllRecords()
@@ -26,7 +73,7 @@ RecordsRouter.get('/',
         res.sendStatus(500)
         return
     })
-
+// получение всех заказов (осуществ., и неосущ.,) определенного клиента
 RecordsRouter.get('/:phoneNumber',
     async( req: Request, res: Response ) => {
         const recordByPhone = await recordsRepo.getRecordsByPhone(req.params.phoneNumber)
@@ -40,7 +87,7 @@ RecordsRouter.get('/:phoneNumber',
         return
 
     })
-
+// обновление данных о записи клиента в салон, доделать логику валидации
 RecordsRouter.put('/:phoneNumber',
     putRecordsValidation,
     checkForErrors,
@@ -61,7 +108,8 @@ RecordsRouter.put('/:phoneNumber',
         }
         res.sendStatus(404)// todo перед отправкой смс должна пройти валидация на существование номера в БД
         return
-    })// запись на прием
+    })
+// запись на прием
 RecordsRouter.post('/',
     postRecordsValidation,
     checkForErrors,
